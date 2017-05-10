24b7d4d548803ba3008b63e184c9b5bd48835746
Drew Pham <drewvpham@gmail.com>
2017-05-10 08:35:20 -0500

    completed

---
+++ dic.py
@@ -0,0 +1,11 @@
+info={'name':'Drew', 'age': '24', 'country of birth': 'Americaaaa', 'favorite language':'engrish'}
+
+
+def hash(info):
+        print 'My name is {}'.format(info['name'])
+        print 'My age is {}'.format(info['age'])
+        print 'My country of birth is the {}'.format(info['country of birth'])
+        print 'My favorite language is {}'.format(info['favorite language'])
+        
+
+hash(info)
